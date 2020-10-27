f04916b7e95e83db45b21db35e1063b16a0dc255
VadStugen <53822162+Vadstugen@users.noreply.github.com>
2020-10-27 22:05:37 +0900

    Create 2750.java

---
+++ 2750.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class Main{
+    public static void main(String []args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        int temp = 0;
+        for(int i=0; i<n; i++){
+            arr[i] = sc.nextInt();
+        }
+        for(int i=0; i<n-1; i++){
+            for(int j=0; j<n-1; j++){
+                if(arr[j] > arr[j+1]){
+                    temp = arr[j+1];
+                    arr[j+1] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        for(int i=0; i<n; i++){
+            System.out.println(arr[i]);
+        }
+    }
+}
